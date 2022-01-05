ec4a0f5c8572ab42c08589b6ed0d25f10079039c
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2022-01-05 21:27:52 +0530

    Hash the passwords.

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
-const auth = require("../middleware/auth");
 const _ = require("lodash");
-const { User, validate } = require("../models/user");
+const bcrypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
+const { User, validate } = require("../models/user");
 
 router.get("/me", auth, async (req, res) => {
   const user = await User.findById(req.user._id).select("-password");
@@ -16,14 +16,9 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered.");
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
-  await user.save();
-
   user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
   res.send(_.pick(user, ["_id", "name", "email"]));
