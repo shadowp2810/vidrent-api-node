fd09c4c1e4122ac247cb29f9d059d0b4acd91a1a
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2022-01-05 21:34:53 +0530

    Authenticate users.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+const app = express();
+const express = require("express");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
-const genres = require("./routes/genres");
+const auth = require("./routes/auth");
 const customers = require("./routes/customers");
+const genres = require("./routes/genres");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
-const express = require("express");
-const app = express();
 
 mongoose
   .connect("mongodb://localhost/vidly")
@@ -20,6 +21,7 @@ app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
