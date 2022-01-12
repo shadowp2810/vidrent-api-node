d0a0672cf262aa4aed8fd6f5744e8fbb0d53777c
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2022-01-12 20:55:55 +0530

    TDD integration test returns route auth

---
+++ tests/integration/routes/returns.test.js
@@ -0,0 +1,67 @@
+const moment = require("moment");
+const request = require("supertest");
+const { Rental } = require("../../models/rental");
+const { Movie } = require("../../models/movie");
+const { User } = require("../../models/user");
+const mongoose = require("mongoose");
+
+describe("/api/returns", () => {
+  let server;
+  let customerId;
+  let movieId;
+  let rental;
+  let movie;
+  let token;
+
+  const exec = () => {
+    return request(server)
+      .post("/api/returns")
+      .set("x-auth-token", token)
+      .send({ customerId, movieId });
+  };
+
+  beforeEach(async () => {
+    server = require("../../index");
+
+    customerId = mongoose.Types.ObjectId();
+    movieId = mongoose.Types.ObjectId();
+    token = new User().generateAuthToken();
+
+    movie = new Movie({
+      _id: movieId,
+      title: "12345",
+      dailyRentalRate: 2,
+      genre: { name: "12345" },
+      numberInStock: 10,
+    });
+    await movie.save();
+
+    rental = new Rental({
+      customer: {
+        _id: customerId,
+        name: "12345",
+        phone: "12345",
+      },
+      movie: {
+        _id: movieId,
+        title: "12345",
+        dailyRentalRate: 2,
+      },
+    });
+    await rental.save();
+  });
+
+  afterEach(async () => {
+    await server.close();
+    await Rental.remove({});
+    await Movie.remove({});
+  });
+
+  it("should return 401 if client is not logged in", async () => {
+    token = "";
+
+    const res = await exec();
+
+    expect(res.status).toBe(401);
+  });
+});
