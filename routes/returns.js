fd79e9ef890ece34e5faa4f2d3aabdeef4646d96
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2022-01-12 21:54:51 +0530

    Refactor validate middleware.

--- routes/returns.js
+++ routes/returns.js
@@ -1,37 +1,39 @@
-const moment = require("moment");
+const Joi = require("joi");
+const validate = require("../middleware/validate");
 const { Rental } = require("../models/rental");
 const { Movie } = require("../models/movie");
 const auth = require("../middleware/auth");
 const express = require("express");
 const router = express.Router();
 
-router.post("/", auth, async (req, res) => {
-  if (!req.body.customerId)
-    return res.status(400).send("customerId not provided");
-  if (!req.body.movieId) return res.status(400).send("movieId not provided");
-  res.status(401).send("Unauthorized");
+router.post("/", [auth, validate(validateReturn)], async (req, res) => {
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
 
-  const rental = await Rental.findOne({
-    "customer._id": req.body.customerId,
-    "movie._id": req.body.movieId,
-  });
-  if (!rental) return res.status(404).send("Rental not found");
+  if (!rental) return res.status(404).send("Rental not found.");
+
   if (rental.dateReturned)
-    return res.status(400).send("Return already processed");
+    return res.status(400).send("Return already processed.");
 
-  rental.dateReturned = new Date();
-  const rentalDays = moment().diff(rental.dateOut, "days");
-  rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
+  rental.return();
   await rental.save();
 
-  await Movie.updateOne(
+  await Movie.update(
     { _id: rental.movie._id },
     {
       $inc: { numberInStock: 1 },
     }
   );
 
-  return res.status(200).send(rental);
+  return res.send(rental);
 });
+
+function validateReturn(req) {
+  const schema = {
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required(),
+  };
+
+  return Joi.validate(req, schema);
+}
 
 module.exports = router;
