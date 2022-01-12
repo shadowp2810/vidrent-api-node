3a57ae42f65eb643505c5a6565ec2740da16f3db
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2022-01-12 21:13:51 +0530

    TDD input and lookup rentals route.

--- tests/integration/routes/returns.test.js
+++ tests/integration/routes/returns.test.js
@@ -64,4 +64,28 @@ describe("/api/returns", () => {
 
     expect(res.status).toBe(401);
   });
+
+  it("should return 400 if customerId is not provided", async () => {
+    customerId = "";
+
+    const res = await exec();
+
+    expect(res.status).toBe(400);
+  });
+
+  it("should return 400 if movieId is not provided", async () => {
+    movieId = "";
+
+    const res = await exec();
+
+    expect(res.status).toBe(400);
+  });
+
+  it("should return 404 if no rental found for the customer/movie", async () => {
+    await Rental.remove({});
+
+    const res = await exec();
+
+    expect(res.status).toBe(404);
+  });
 });
